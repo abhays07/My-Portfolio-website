0ce04f7d69652e7483b6328f7354c5856bfd6b11
abhays07 <itsrajputabhaysingh997@gmail.com>
2025-09-18 01:06:25 +0530

    remove example.com website

--- client/src/data/project.js
+++ client/src/data/project.js
@@ -5,7 +5,7 @@ import feane from "../assets/images/feane.png";
 export const CATEGORIES = ["Show All","Frontend","Mern Stack",];
 
 export const PROJECTS = [
-  { id:"p1", title:"Event Planner Website", tag:"Mern Stack", cover:eventImage, href:"https://example.com/app", repo:"https://github.com/abhays07/EventPlanner.git" },
+  { id:"p1", title:"Event Planner Website", tag:"Mern Stack", cover:eventImage, href:"", repo:"https://github.com/abhays07/EventPlanner.git" },
   { id:"p2", title:"Portfolio Website", tag:"Frontend", cover:portfolioImage, href:"https://abhay-singh-lodhi.netlify.app/", repo:"https://github.com/me/website" },
   { id:"p3", title:"Feane - Landing Page", tag:"Frontend", cover:feane, href:"https://abhays07.github.io/feaneProject/", repo:"https://github.com/abhays07/feaneProject.git"},
 ];
